e9b54a8b0801eb8de112aa4dcea4d14f3f6148c1
jrofurtado@fear-airsoft.com <>
2013-03-04 11:26:11 +0000

    Refactor

---
+++ src/main/java/com/fear_airsoft/json/WeatherIcon.java
@@ -0,0 +1,13 @@
+package com.fear_airsoft.json;
+
+class WeatherIcon {
+    private String value;
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+}
